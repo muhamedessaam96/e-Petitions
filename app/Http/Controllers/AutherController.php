7acfe0b39422e9db7ec220a73bc49b412aa4fd22
muhamedessam <muhamedessam96@hotmail.com>
2023-02-20 19:58:50 +0200

    - Creating an author resource infrastructure. - Creating an author resource infrastructure.

---
+++ app/Http/Controllers/AutherController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Auther;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AutherController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        //
+    }
+
+
+    public function show(Auther $auther): Response
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Auther $auther): Response
+    {
+        //
+    }
+
+}
